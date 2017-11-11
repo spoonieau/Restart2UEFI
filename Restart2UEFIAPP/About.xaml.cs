d1b4824f43cbdc8ec2e84aef1ddb29ba11a6cc67
Spoonieau <spoonie.woonie@gmail.com>
2017-11-11 14:57:51 +0800

    Initial upload

---
+++ Restart2UEFIAPP/About.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.Resources;
+using System.Reflection;
+using Windows.UI.Xaml.Documents;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Restart2UEFIAPP
+{
+    public sealed partial class About : ContentDialog
+    {
+        public About()
+        {
+            this.InitializeComponent();
+            var resourceLoader = ResourceLoader.GetForCurrentView();
+
+            this.Title = resourceLoader.GetString("AppDisplayName") + " " + (typeof(App).GetTypeInfo().Assembly.GetName().Version).ToString();
+
+            txtAbout.FontSize = 10;
+            txtAbout.Text = resourceLoader.GetString("AboutInfo") + " ";
+
+            Hyperlink hyperlink = new Hyperlink();
+            Run gitLink = new Run();
+            gitLink.Text = "https://github.com/spoonieau/Restart2UEFI";
+            hyperlink.NavigateUri = new Uri("https://github.com/spoonieau/Restart2UEFI");
+            hyperlink.Inlines.Add(gitLink);
+            txtAbout.Inlines.Add(hyperlink);
+
+
+        }
+
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+
+        
+    }
+}
---
+++ Restart2UEFIAPP/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Restar2UEFIAPP UWP build 1.1.0.0
+ * 
+ * Restart2UEFI winforms build ported to UWP.
+ * Was going to be release on the windows store but due to needing the ues of a win32
+ * exe and only holding a developer licence I was unable to submit.
+ * 
+ * I win32 dir is is the helper win32 (Restart2UEFIHelper.exe), which is need to make the 
+ * OS calls out side the WR sand box.
+ * 
+ * Written by SpoonieAU
+ * https://github.com/spoonieau/Restart2UEFI
+ * Released under GPL3
+ */
+
+using System;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.ApplicationModel;
+using Windows.UI.ViewManagement;
+using System.Runtime.InteropServices;
+using Windows.ApplicationModel.Resources;
+using Windows.ApplicationModel.AppService;
+using Windows.UI.Popups;
+using Windows.UI;
+using Windows.UI.Xaml.Navigation;
+
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Restart2UEFIAPP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(395, 250));
+            ApplicationView.PreferredLaunchViewSize = new Size(395, 250);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+
+            var resourceLoader = ResourceLoader.GetForCurrentView();
+
+            tblRestartButton.Text = resourceLoader.GetString("btnFirmwareRestart");
+            tblShutdownButton.Text = resourceLoader.GetString("btnFirmwareShutdown");
+
+        }
+
+        private void btnAbout_Click(object sender, RoutedEventArgs e)
+        {
+            AboutAsync();
+        }
+
+        private async void btnFirmwareRestart_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync("restart");
+            
+        }
+
+        private async void btnFirmwareShutdown_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync("shutdown");
+        }
+
+        private async void AboutAsync()
+        {
+            About about = new About();
+            await about.ShowAsync();
+        }
+    }
+}
