cd4cc7985f4d4e672c435927ba80fe47276d9e6e
Spoonieau <spoonie.woonie@gmail.com>
2017-11-11 14:54:56 +0800

    Initial upload

---
+++ Restart2UEFIHelper/frmMain.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Restart2UEFIHelper
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirmware = new System.Windows.Forms.TextBox();
+            this.txtArgs = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtFirmware
+            // 
+            this.txtFirmware.Location = new System.Drawing.Point(12, 12);
+            this.txtFirmware.Name = "txtFirmware";
+            this.txtFirmware.Size = new System.Drawing.Size(126, 20);
+            this.txtFirmware.TabIndex = 0;
+            // 
+            // txtArgs
+            // 
+            this.txtArgs.Location = new System.Drawing.Point(12, 47);
+            this.txtArgs.Name = "txtArgs";
+            this.txtArgs.Size = new System.Drawing.Size(126, 20);
+            this.txtArgs.TabIndex = 1;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(150, 84);
+            this.Controls.Add(this.txtArgs);
+            this.Controls.Add(this.txtFirmware);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Restart2FirmwareHelper";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirmware;
+        private System.Windows.Forms.TextBox txtArgs;
+    }
+}
+
---
+++ Restart2UEFIHelper/frmMain.cs
@@ -0,0 +1,152 @@
+﻿/*
+ * Restar2UEFIHelper build 1.0.0.0
+ * 
+ * Healper exe need by Restart2UEFIAPP. The app luanches and passes args
+ * to this helper, then the helper makes to calls to the OS.
+ * 
+ * https://github.com/spoonieau/Restart2UEFI
+ * Written by SpoonieAU
+ * Released under GPL3
+ */
+
+
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace Restart2UEFIHelper
+{
+    public partial class frmMain : Form
+    {
+        public class FirmwareType
+        {
+            [DllImport("kernel32.dll")]
+            static extern bool GetFirmwareType(ref uint FirmwareType);
+
+            public static uint GetFirmwareType()
+            {
+                uint firmwaretype = 0;
+                if (GetFirmwareType(ref firmwaretype))
+                    return firmwaretype;
+                else
+                    return 0;
+            }
+        }
+
+        string[] args = Environment.GetCommandLineArgs();
+        string fwType = "";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void firmWareUEFI()
+        {   //args pass from fulltrustLuancher at pos 3
+            switch (args.ElementAt(3))
+            {
+                case "/fwr":
+                    try
+                    {
+                        //Start shutdown.exe pass args /s (shutdown) + /fw (set firmware flag) + /t 0 (no wait time)
+                        Process firmwareShutdown = new Process();
+                        firmwareShutdown.StartInfo.FileName = "shutdown.exe";
+                        firmwareShutdown.StartInfo.Arguments = "/s /fw /t 0";
+                        firmwareShutdown.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        firmwareShutdown.StartInfo.UseShellExecute = true;
+                        firmwareShutdown.StartInfo.Verb = "runas";
+                        firmwareShutdown.Start();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Unable to start shutdown process. Click OK to exit.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Application.Exit();
+                    }
+                    break;
+
+                case "/fws":
+                    try
+                    {
+                        //Start shutdown.exe pass args /r (restart) + /fw (set firmware flag) + /t 0 (no wait time)
+                        Process firmwareRestart = new Process();
+                        firmwareRestart.StartInfo.FileName = "shutdown.exe";
+                        firmwareRestart.StartInfo.Arguments = "/r /fw /t 0";
+                        firmwareRestart.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        firmwareRestart.StartInfo.UseShellExecute = true;
+                        firmwareRestart.StartInfo.Verb = "runas";
+                        firmwareRestart.Start();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Unable to start restart process. Click OK to exit.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Application.Exit();
+                    }
+                    break;
+                    
+            }
+
+        }
+
+        private void errorRetrive()
+        {
+            txtFirmware.Text ="Retrived fw type:" + fwType;
+            txtArgs.Text = "Args passed: " + args.ElementAt(3);
+
+            MessageBox.Show("UEFI type has returned as non UEFI, Application will now exit.", "Error Retrieving Type", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Application.Exit();
+        }
+
+        private void errorNotUefi()
+        {
+            txtFirmware.Text = "Retrived fw type:" + fwType;
+            txtArgs.Text = "Args passed: " + args.ElementAt(3);
+
+            MessageBox.Show("Unable to retrive UEFI type, Application will now exit.", "Error Incorrect Type", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Application.Exit();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //Get firmware return type FirmwareTypeUnknown  = 0, FirmwareTypeBios = 1, FirmwareTypeUefi = 2, FirmwareTypeMax = 3
+            int fwareT = int.Parse(FirmwareType.GetFirmwareType().ToString());
+
+            fwType = fwareT.ToString();
+
+            try
+            {
+
+                
+                switch (fwareT)
+                {
+                    case 0:
+                        errorNotUefi();
+                        break;
+
+                    case 1:
+                        errorNotUefi();
+                        break;
+
+                    case 2:
+
+                        firmWareUEFI();
+                        break;
+
+                    case 3:
+                        errorNotUefi();
+                        break;
+
+                    default:
+                        errorRetrive();
+                        break;
+                }
+            }
+            catch
+            {
+                errorRetrive();
+            }
+        }
+    }
+}
