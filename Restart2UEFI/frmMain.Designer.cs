1c45d1211506152d5f74d321bcf8904f0f82749a
Spoonieau <spoonie.woonie@gmail.com>
2017-11-11 15:08:58 +0800

    Initial upload

---
+++ Restart2UEFI/frmMain.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Restart2UEFI
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.btnFirmware = new System.Windows.Forms.Button();
+            this.btnFirmwareRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbout.Location = new System.Drawing.Point(12, 12);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(30, 30);
+            this.btnAbout.TabIndex = 2;
+            this.btnAbout.Text = "?";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStatus.Location = new System.Drawing.Point(49, 12);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(318, 29);
+            this.txtStatus.TabIndex = 3;
+            // 
+            // btnFirmware
+            // 
+            this.btnFirmware.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnFirmware.Image = global::Restart2UEFI.Properties.Resources.ICicon128;
+            this.btnFirmware.Location = new System.Drawing.Point(12, 49);
+            this.btnFirmware.Name = "btnFirmware";
+            this.btnFirmware.Size = new System.Drawing.Size(170, 170);
+            this.btnFirmware.TabIndex = 4;
+            this.btnFirmware.Text = "Set Boot To Firmware Falg and Shutdown";
+            this.btnFirmware.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnFirmware.UseVisualStyleBackColor = true;
+            this.btnFirmware.Click += new System.EventHandler(this.btnFirmware_Click);
+            // 
+            // btnFirmwareRestart
+            // 
+            this.btnFirmwareRestart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnFirmwareRestart.Image = global::Restart2UEFI.Properties.Resources.ICiconRestart128;
+            this.btnFirmwareRestart.Location = new System.Drawing.Point(197, 49);
+            this.btnFirmwareRestart.Name = "btnFirmwareRestart";
+            this.btnFirmwareRestart.Size = new System.Drawing.Size(170, 170);
+            this.btnFirmwareRestart.TabIndex = 5;
+            this.btnFirmwareRestart.Text = "Set Boot to Frimware Falg and Restart ";
+            this.btnFirmwareRestart.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnFirmwareRestart.UseVisualStyleBackColor = true;
+            this.btnFirmwareRestart.Click += new System.EventHandler(this.btnFirmwareRestart_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 231);
+            this.Controls.Add(this.btnFirmwareRestart);
+            this.Controls.Add(this.btnFirmware);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnAbout);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Button btnFirmware;
+        private System.Windows.Forms.Button btnFirmwareRestart;
+    }
+}
+
---
+++ Restart2UEFI/frmMain.cs
@@ -0,0 +1,168 @@
+﻿/*
+ * Restar2UEFI winforms build 1.0.0.0
+ * 
+ * https://github.com/spoonieau/Restart2UEFI
+ * Written by SpoonieAU
+ * Released under GPL3
+ */
+
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace Restart2UEFI
+{
+    public partial class frmMain : Form
+    {
+        public class FirmwareType
+        {
+            [DllImport("kernel32.dll")]
+            static extern bool GetFirmwareType(ref uint FirmwareType);
+
+            public static uint GetFirmwareType()
+            {
+                uint firmwaretype = 0;
+                if (GetFirmwareType(ref firmwaretype))
+                    return firmwaretype;
+                else
+                    return 0;
+            }
+        }
+        string fwType = "";
+        public frmMain()
+        {
+            InitializeComponent();
+
+            this.Text = "Restart2UEFI(WinForms)" + (typeof(Program).GetTypeInfo().Assembly.GetName().Version).ToString();
+
+            try
+            {
+                int fwareT = int.Parse(FirmwareType.GetFirmwareType().ToString());
+                switch (fwareT)
+                {
+                    case 0:
+                        errorNotUefi();
+                        fwType = "0";
+                        break;
+
+                    case 1:
+                        errorNotUefi();
+                        fwType = "1";
+                        break;
+
+                    case 2:
+                        firmWareUEFI();
+                        break;
+
+                    case 3:
+                        errorNotUefi();
+                        fwType = "3";
+                        break;
+
+                    default:
+                        errorRetrive();
+                        break;
+                }
+            }
+            catch
+            {
+                errorRetrive();
+            }
+               
+
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Restart2UEFI(WinForms)" + (typeof(Program).GetTypeInfo().Assembly.GetName().Version).ToString() + Environment.NewLine +
+                            "A App using inbult system functions to shutdown or restart you UEFI system to Firmware" + Environment.NewLine +
+                            "Developed by SpoonieAU " + "https://github.com/spoonieau","Restart2UEFI",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void errorRetrive()
+        {
+            txtStatus.BackColor = Color.White;
+            txtStatus.ForeColor = Color.Red;
+            txtStatus.ReadOnly = true;
+            txtStatus.Text = "Firmware type (" + fwType + "): UEFI Not Present";
+
+            btnFirmware.Enabled = false;
+            btnFirmwareRestart.Enabled = false;
+
+            MessageBox.Show("An error has occurred trying to retrive Firmware Type", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void errorNotUefi()
+        {
+            txtStatus.BackColor = Color.White;
+            txtStatus.ForeColor = Color.Red;
+            txtStatus.Text = "FirmWare type (" + fwType + "): UEFI Not Present";
+
+            btnFirmware.Enabled = false;
+            btnFirmwareRestart.Enabled = false;
+
+            MessageBox.Show("Firmware has not been retrieved as UEFI", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+             
+        }
+
+        private void firmWareUEFI()
+        {
+            txtStatus.BackColor = Color.White;
+            txtStatus.ForeColor = Color.Green;
+            txtStatus.ReadOnly = true;
+            txtStatus.Text = "Firmware type (2): UEFI Present";
+        }
+
+        private void btnFirmware_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process firmware = new Process();
+                firmware.StartInfo.FileName = "shutdown.exe";
+                firmware.StartInfo.Arguments = "/s /fw /t 0";
+                firmware.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                firmware.Start();
+                firmware.WaitForExit();
+
+                //Process.Start("shutdown", "/fw /t 0");
+
+                MessageBox.Show("Firmware Flag set", "Flag Set", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Failure to Set Firmware/Shutdown Flag" + Environment.NewLine
+                        + "Application will now exit", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                endApp(); 
+            }
+        }
+
+        private void btnFirmwareRestart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process firmwareRestart = new Process();
+                firmwareRestart.StartInfo.FileName = "shutdown.exe";
+                firmwareRestart.StartInfo.Arguments = "/r /fw /t 0";
+                firmwareRestart.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                firmwareRestart.Start();
+
+                //Process.Start("shutdown", "/r /fw /t 0");
+            }
+            catch
+            {
+                MessageBox.Show("Failure to Set Firmware/Restart Flag" + Environment.NewLine
+                        + "Application will now exit", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+        private void endApp()
+        {
+            Application.Exit();
+        }
+    }
+
+}
